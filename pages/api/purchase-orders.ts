266877e61df7e988a88555c19e5109ee1bcc4336
ku28 <ku28sh07@gmail.com>
2025-12-02 05:04:13 +0530

    fix bugs

--- pages/api/purchase-orders.ts
+++ pages/api/purchase-orders.ts
@@ -10,7 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     
     if (req.method === 'GET') {
         try {
-            const selectedDoctorId = req.query.doctorId ? Number(req.query.doctorId) : undefined
+            const selectedDoctorId = req.query.doctorId ? Number(req.query.doctorId) : null
             const purchaseOrders = await prisma.purchaseOrder.findMany({
                 where: getDoctorFilter(user, selectedDoctorId),
                 orderBy: { orderDate: 'desc' },
